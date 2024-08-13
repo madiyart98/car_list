a90d6e19d1d0bf5630c202e0b0e476d343938d2c
t.madiyar <aizhir.mt@gmail.com>
2024-08-13 22:12:23 +0500

    add get by filter

--- app/api/endpoints.py
+++ app/api/endpoints.py
@@ -1,6 +1,8 @@
 # app/api/endpoints.py
 
-from fastapi import APIRouter, Depends, HTTPException
+from typing import Optional
+
+from fastapi import APIRouter, Depends, HTTPException, Query
 from sqlalchemy.orm import Session
 
 from .. import crud, models, schemas
@@ -9,9 +11,9 @@ from ..database import get_db
 router = APIRouter()
 
 
-@router.get("/car_info", response_model=schemas.Car)
+@router.get("/car_info", response_model=list[schemas.Car])
 def get_car_info(model: str, db: Session = Depends(get_db)):
-  car = crud.get_car_by_model(db, model=model)
+  car = crud.get_cars_by_model(db, model=model)
   if not car:
     raise HTTPException(status_code=404, detail="Car not found")
   return car
@@ -28,3 +30,15 @@ def update_car(car_id: int, car: schemas.CarCreate, db: Session = Depends(get_db
   if not updated_car:
     raise HTTPException(status_code=404, detail="Car not found")
   return updated_car
+
+
+@router.get("/car_info/", response_model=list[schemas.Car])
+def filter_cars(
+    year: Optional[int] = Query(None, description="Filter by year of manufacture"),
+    body_type: Optional[str] = Query(None, description="Filter by body type"),
+    db: Session = Depends(get_db)
+):
+  cars = crud.get_cars_by_filters(db, year, body_type)
+  if not cars:
+    raise HTTPException(status_code=404, detail="No cars found matching the criteria")
+  return cars
--- app/crud.py
+++ app/crud.py
@@ -1,12 +1,30 @@
 # app/crud.py
 
+from sqlalchemy import or_
 from sqlalchemy.orm import Session
 
 from . import models, schemas
 
 
-def get_car_by_model(db: Session, model: str):
-  return db.query(models.Car).filter(models.Car.model == model).first()
+def get_cars_by_model(db: Session, model: str):
+  return db.query(models.Car).filter(
+      or_(
+          models.Car.model.ilike(f'%{model}%'),
+          models.Car.mark.ilike(f'%{model}%')
+      )
+  ).all()
+
+
+def get_cars_by_filters(db: Session, year: int | None = None, body_type: str | None = None):
+  query = db.query(models.Car)
+
+  if year is not None:
+    query = query.filter(models.Car.year == year)
+
+  if body_type is not None:
+    query = query.filter(models.Car.body_type.ilike(f'%{body_type}%'))
+
+  return query.all()
 
 
 def create_car(db: Session, car: schemas.CarCreate):
