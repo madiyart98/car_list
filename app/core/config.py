90b6df5965fcfc25b7b7ee770a0cc47abde5dcd7
madiyart98 <aizhir.mt@gmail.com>
2024-08-15 11:09:21 +0500

    fix default port

--- app/core/config.py
+++ app/core/config.py
@@ -11,7 +11,7 @@ class Settings:
   DATABASE_USER: str = os.getenv("DATABASE_USER", "postgres")
   DATABASE_PASSWORD: str = os.getenv("DATABASE_PASSWORD", "password")
   DATABASE_HOST: str = os.getenv("DATABASE_HOST", "localhost")
-  DATABASE_PORT: str = os.getenv("DATABASE_PORT", "5433")
+  DATABASE_PORT: str = os.getenv("DATABASE_PORT", "5432")
   DEBUG: bool = os.getenv("DEBUG", "False").lower() in ("true", "1", "t")
   DATABASE_URL = f'postgresql+psycopg2://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}'
   TOKEN_VERIFICATION_KEY: str = os.getenv("TOKEN_VERIFICATION_KEY", "")
